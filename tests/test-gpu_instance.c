c83fe80a9fa700bb09600308e384910d83816485
marceline-cramer <cramermarceline@gmail.com>
2021-05-30 21:23:30 -0600

    Add mdo_gpu_instance_log_result() tests

--- tests/test-gpu_instance.c
+++ tests/test-gpu_instance.c
@@ -37,11 +37,37 @@ test_create_and_destroy (void **state)
   assert_int_equal (0, teardown (state));
 }
 
+static void
+test_log_result_success_code (void **state)
+{
+  static const VkResult SUCCESS_CODE = VK_SUCCESS;
+
+  mdo_gpu_instance_t *instance = (mdo_gpu_instance_t *)*state;
+  mdo_result_t result
+      = mdo_gpu_instance_log_result (instance, SUCCESS_CODE, "");
+  assert_true (mdo_result_success (result));
+}
+
+static void
+test_log_result_error_code (void **state)
+{
+  /* the exact error code doesn't matter;
+   * VK_ERROR_UNKNOWN was picked arbitrarily
+   * TODO(marceline-cramer): Test all Vulkan error codes? */
+  static const VkResult ERROR_CODE = VK_ERROR_UNKNOWN;
+
+  mdo_gpu_instance_t *instance = (mdo_gpu_instance_t *)*state;
+  mdo_result_t result = mdo_gpu_instance_log_result (instance, ERROR_CODE, "");
+  assert_false (mdo_result_success (result));
+}
+
 int
 main (void)
 {
   const struct CMUnitTest tests[] = {
     cmocka_unit_test_setup_teardown (test_create_and_destroy, NULL, NULL),
+    cmocka_unit_test (test_log_result_success_code),
+    cmocka_unit_test (test_log_result_error_code),
   };
 
   return cmocka_run_group_tests (tests, setup, teardown);
