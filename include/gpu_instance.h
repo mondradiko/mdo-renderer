1bbfe10d10efaa61de0a2e42088969c70a1eca62
marceline-cramer <cramermarceline@gmail.com>
2021-05-30 21:21:09 -0600

    Fix use of "mdo_vk_instance_t" in gpu_instance.h docs

--- include/gpu_instance.h
+++ include/gpu_instance.h
@@ -15,7 +15,7 @@ typedef struct mdo_gpu_instance_s mdo_gpu_instance_t;
 
 /** @function mdo_gpu_instance_create
   @brief Creates a GPU instance.
-  @param instance The new #mdo_vk_instance_t.
+  @param instance The new #mdo_gpu_instance_t.
   @param alloc The allocator to use.
   @return #mdo_result_t.
  */
@@ -24,7 +24,7 @@ mdo_result_t mdo_gpu_instance_create (mdo_gpu_instance_t **,
 
 /** @function mdo_gpu_instance_delete
   @brief Deletes a GPU instance.
-  @param instance The #mdo_vk_instance_t to delete.
+  @param instance The #mdo_gpu_instance_t to delete.
  */
 void mdo_gpu_instance_delete (mdo_gpu_instance_t *);
 
