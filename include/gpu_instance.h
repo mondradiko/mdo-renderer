b2a73492f3c902e872a308bee50fb42788b7e14b
marceline-cramer <cramermarceline@gmail.com>
2021-05-30 21:21:39 -0600

    Declare mdo_gpu_instance_log_result()

--- include/gpu_instance.h
+++ include/gpu_instance.h
@@ -5,6 +5,8 @@
 
 #pragma once
 
+#include <vulkan/vulkan.h>
+
 #include "mdo-utils/allocator.h"
 #include "mdo-utils/result.h"
 
@@ -28,3 +30,12 @@ mdo_result_t mdo_gpu_instance_create (mdo_gpu_instance_t **,
  */
 void mdo_gpu_instance_delete (mdo_gpu_instance_t *);
 
+/** @function mdo_gpu_instance_log_result
+  @brief Formats and logs a Vulkan result into an #mdo_result_t.
+  @brief instance The #mdo_gpu_instance_t to format a result from.
+  @brief result_code The Vulkan result code.
+  @brief error_message A brief error message to log in the case of an error.
+  @return #mdo_result_t.
+ */
+mdo_result_t mdo_gpu_instance_log_result (mdo_gpu_instance_t *, VkResult,
+                                          const char *);
